df6314b8577c61531438ccec5c421c311f97b8c1
Ankita Patidar <ankita.patidar@ayanworks.com>
2025-07-21 15:44:40 +0530

    feat: schema support

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,6 +6,7 @@ import {
   VERIFICATION_METHOD_TYPE_ECDSA_SECP256K1_VERIFICATION_KEY_2019,
   VerificationMethod,
 } from '@credo-ts/core'
+import bs58 from 'bs58'
 
 import { SECURITY_CONTEXT_SECP256k1_RECOVERY_URL } from '../signature-suites'
 
@@ -43,4 +44,42 @@ export function getSecp256k1DidDocWithPublicKey(
   didDocumentBuilder.addKeyAgreement(verificationMethod.id)
 
   return didDocumentBuilder.build()
+}
+
+/**
+ * Converts a hex-encoded public key to base58-encoded format
+ * @param publicKeyHex - Hex string (e.g. from EcdsaSecp256k1VerificationKey2019)
+ * @returns base58-encoded public key string
+ */
+export function convertHexToBase58(publicKeyHex: string): string {
+  // Remove potential 0x prefix
+  if (publicKeyHex.startsWith('0x')) {
+    publicKeyHex = publicKeyHex.slice(2)
+  }
+
+  // Convert hex to a buffer
+  const buffer = Buffer.from(publicKeyHex, 'hex')
+
+  const publicKeyBase58 = bs58.encode(buffer)
+  console.log('publicKeyBase58 in convertHexToBase58------', publicKeyBase58)
+
+  return publicKeyBase58
+}
+
+export function parseAddress(address: string) {
+  const parts = address.split(':')
+  if (parts.length < 3) {
+    throw new Error('Invalid CAIP format')
+  }
+  return parts[2]
+}
+
+export function getPreferredKey(methods: VerificationMethod[]): string {
+  for (const m of methods) {
+    if (m.blockchainAccountId) return m.blockchainAccountId
+    if (m.publicKeyHex) return m.publicKeyHex
+  }
+
+  // Guaranteed fallback
+  return methods[0].controller
 }
